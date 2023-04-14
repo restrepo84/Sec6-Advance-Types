3a2ce37a39acbe78b11cc3ca70795252d6999382
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-04-14 15:32:18 -0600

    overloads optional chaining nullish coalescing

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a;
 const e1 = {
     name: "Max",
     privileges: ["create-server"],
@@ -10,57 +11,15 @@ function add(a, b) {
     }
     return a + b;
 }
-function printEmployeeInformation(emp) {
-    console.log("Name: " + emp.name);
-    if ("privileges" in emp) {
-        console.log("Privileges: " + emp.privileges);
-    }
-    if ("startDate" in emp) {
-        console.log("Start Date: " + emp.startDate);
-    }
-}
-printEmployeeInformation({ name: "Manu", startDate: new Date() });
-class Car {
-    drive() {
-        console.log("Driving...");
-    }
-}
-class Truck {
-    drive() {
-        console.log("Driving a truck...");
-    }
-    loadCargo(amount) {
-        console.log("Loading cargo ..." + amount);
-    }
-}
-const v1 = new Car();
-const v2 = new Truck();
-function useVehicle(vehicle) {
-    vehicle.drive();
-    if (vehicle instanceof Truck) {
-        vehicle.loadCargo(1000);
-    }
-}
-useVehicle(v1);
-useVehicle(v2);
-function moveAnimal(animal) {
-    let speed;
-    switch (animal.type) {
-        case "bird":
-            speed = animal.flyingSpeed;
-            break;
-        case "horse":
-            speed = animal.runningSpeed;
-    }
-    console.log("Moving at speed: " + speed);
-}
-moveAnimal({ type: "bird", flyingSpeed: 10 });
-const userInputElement = document.getElementById("user-input");
-if (userInputElement) {
-    userInputElement.value = "Hi there!";
-}
-const errorBag = {
-    email: "Not a valid email!",
-    username: "Must start with a capital character!",
+const result = add("Steve", " Restrepo");
+result.split(" ");
+const fetchedUserData = {
+    id: "u1",
+    name: "Steve",
+    job: { title: "CEO", description: "My own company" },
 };
+console.log((_a = fetchedUserData === null || fetchedUserData === void 0 ? void 0 : fetchedUserData.job) === null || _a === void 0 ? void 0 : _a.title);
+const userInput = undefined;
+const storedData = userInput !== null && userInput !== void 0 ? userInput : "DEFAULT";
+console.log(storedData);
 //# sourceMappingURL=app.js.map
